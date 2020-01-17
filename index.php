1931e3f41022556bd6831b1ea02e3d4d0fa21275
Aakash Sharma <sharma1032000@gmail.com>
2020-01-17 16:00:54 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portofolio Website</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+	<div>
+		<h1>Hello rveryone</h1>
+	</div>
+</body>
+</html>
